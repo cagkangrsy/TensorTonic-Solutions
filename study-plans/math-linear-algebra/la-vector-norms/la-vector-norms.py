d9160706fdae90f881bd89d90ad74329265665f5
Çağkan Gürsoy <75528306+cagkangrsy@users.noreply.github.com>
2026-07-22 10:49:34 +0200

    Solve la-vector-norms

---
+++ study-plans/math-linear-algebra/la-vector-norms/la-vector-norms.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def vector_norms(v):
+    """
+    Returns: float64 array of shape (3,) containing [L1, L2, L-inf] norms.
+    """
+    v = np.asarray(v, dtype=np.float64)
+    return np.array([np.linalg.norm(v, ord=1), np.linalg.norm(v, ord=2), np.linalg.norm(v, ord=np.inf)])
+    pass
